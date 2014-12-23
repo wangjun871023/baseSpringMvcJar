2940294ef58f89500eec4579280007ebf91fcd4d
Administrator <Administrator@AX-wangjun-PC.AX.CLIC>
2014-12-23 09:26:40 +0800

    验证码

--- baseSpringMvcJar/src/main/java/com/macrosoft/controller/VerifyCodeController.java
+++ baseSpringMvcJar/src/main/java/com/macrosoft/controller/VerifyCodeController.java
@@ -3,12 +3,9 @@ package com.macrosoft.controller;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -32,12 +29,8 @@ public class VerifyCodeController {
 		HTTP_HEADERS.setContentType(MediaType.IMAGE_JPEG);
 	}
 
-	private int width;
-	private int height;
-
 	@RequestMapping(value = "/getCodeImage", method = RequestMethod.GET)
-	public ResponseEntity<byte[]> getCodeImage(HttpServletRequest request,
-			HttpServletResponse response, HttpSession session) {
+	public ResponseEntity<byte[]> getCodeImage(HttpSession session,int width,int height) {
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		try {
 			String code = VerifyCodeUtils.outputVerifyImage(width, height, os, 4);
@@ -50,15 +43,5 @@ public class VerifyCodeController {
 			IOUtils.closeQuietly(os);
 		}
 		return null;
-	}
-
-	@Value("100")
-	public void setWidth(int width) {
-		this.width = width;
-	}
-
-	@Value("18")
-	public void setHeight(int height) {
-		this.height = height;
 	}
 }
